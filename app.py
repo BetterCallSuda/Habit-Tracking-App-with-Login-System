ccd247038d769991893e8f3a51da6b2c7812ed16
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-28 23:44:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,3 +36,17 @@ def register():
         return redirect(url_for("login"))
     return render_template("register.html")
 
+
+# ---------------- LOGIN ----------------
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = User.query.filter_by(username=request.form["username"]).first()
+
+        if user and check_password_hash(user.password, request.form["password"]):
+            login_user(user)
+            return redirect(url_for("dashboard"))
+        flash("Invalid credentials")
+
+    return render_template("login.html")
+
