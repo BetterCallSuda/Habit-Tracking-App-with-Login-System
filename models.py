86936ee0f0d2b88eb2538aa8acceb7ff311511f8
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-28 23:42:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,3 +3,15 @@ from flask_login import UserMixin
 from datetime import datetime
 
 db = SQLAlchemy()
+
+
+# -------------------------
+# USER MODEL
+# -------------------------
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    habits = db.relationship("Habit", back_populates="user", cascade="all, delete")
+
